aad44b236c9cd9ae25ac2f142c297a5bf35b01e8
Lorena <111245669+foureyesdev@users.noreply.github.com>
2026-05-08 12:15:32 -0300

    fix: update MembroEquipeRepository to extend JpaRepository

--- portal/src/main/java/br/com/progirls/api/portal/repository/MembroEquipeRepository.java
+++ portal/src/main/java/br/com/progirls/api/portal/repository/MembroEquipeRepository.java
@@ -1,12 +1,12 @@
 package br.com.progirls.api.portal.repository;
 
 import br.com.progirls.api.portal.model.entity.MembroEquipe;
-import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MembroEquipeRepository extends JpaAttributeConverter<MembroEquipe, Long> {
+public interface MembroEquipeRepository extends JpaRepository<MembroEquipe, Long> {
     List<MembroEquipe> findAllByOrderByNomeAsc();
 }
