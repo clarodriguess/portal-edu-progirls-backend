a215cbecc77639e8157f31ff2d331f48737a9b7c
clarisse <clarissebleasby@gmail.com>
2026-06-13 12:45:49 -0300

    feat: atualizar RoadmapMapper com mapeamentos de detalhe

--- portal/src/main/java/br/com/progirls/api/portal/mapper/RoadmapMapper.java
+++ portal/src/main/java/br/com/progirls/api/portal/mapper/RoadmapMapper.java
@@ -1,10 +1,18 @@
 package br.com.progirls.api.portal.mapper;
 
+import br.com.progirls.api.portal.model.dto.conteudo.ConteudoResumoResponseDTO;
+import br.com.progirls.api.portal.model.dto.roadmap.RoadmapConteudoResponseDTO;
+import br.com.progirls.api.portal.model.dto.roadmap.RoadmapDetalheResponseDTO;
 import br.com.progirls.api.portal.model.dto.roadmap.RoadmapResponseDTO;
+import br.com.progirls.api.portal.model.entity.Conteudo;
 import br.com.progirls.api.portal.model.entity.Roadmap;
+import br.com.progirls.api.portal.model.entity.RoadmapConteudo;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface RoadmapMapper {
     RoadmapResponseDTO toDTO(Roadmap roadmap);
+    RoadmapDetalheResponseDTO toDetalheDTO(Roadmap roadmap);
+    RoadmapConteudoResponseDTO toRoadmapConteudoDTO(RoadmapConteudo roadmapConteudo);
+    ConteudoResumoResponseDTO toConteudoResumoDTO(Conteudo conteudo);
 }
