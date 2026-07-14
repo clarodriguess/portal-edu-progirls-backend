91504dc7e99f5c829ce6be89b20fdf4f23938368
clarisse <clarissebleasby@gmail.com>
2026-07-14 20:26:07 -0300

    feat: adicionar JpaSpecificationExecutor no RoadmapRepository

--- portal/src/main/java/br/com/progirls/api/portal/repository/RoadmapRepository.java
+++ portal/src/main/java/br/com/progirls/api/portal/repository/RoadmapRepository.java
@@ -5,7 +5,8 @@ import br.com.progirls.api.portal.model.entity.Roadmap;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
+public interface RoadmapRepository extends JpaRepository<Roadmap, Long>, JpaSpecificationExecutor<Roadmap> {
     Page<Roadmap> findByNivel(NivelRoadmap nivel, Pageable pageable);
 }
